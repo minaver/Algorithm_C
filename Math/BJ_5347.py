c552e1ef0002dfda42b2e5ea747627c8f15ecf70
minaver <psmjoshua@gmail.com>
2022-04-04 21:47:21 +0900

    add:[추가] MATH_LCM

---
+++ Math/BJ_5347.py
@@ -0,0 +1,24 @@
+# Topic : MATH _ LCM
+#
+# PyPy 3 : 108ms
+
+import sys
+
+# 최대 공약수 구하기
+def gdm(a,b):
+  while b != 0:
+    a , b = b , a%b
+  return a
+
+# 최소 공배수 구하기
+def lcm(a,b):
+  return a*b//gdm(a,b)
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+  a , b = map(int,sys.stdin.readline().split())
+  if a >= b:
+    print(lcm(a,b))
+  else:
+    print(lcm(b,a))
