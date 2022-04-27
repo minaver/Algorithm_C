5b21826220b248a661f17dad8f4ea5de6d0051a8
minaver <psmjoshua@gmail.com>
2022-04-27 18:31:48 +0900

    add:[추가] Data_Structure _ 덱

---
+++ Data_Structure/BJ_10866.py
@@ -0,0 +1,47 @@
+# Topic : Data_Structure _ 덱
+#
+# PyPy 3 : 180ms
+
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+dq = deque()
+result = list()
+
+for _ in range(N):
+  cmd = sys.stdin.readline().split()
+  if cmd[0] == "push_front":
+    dq.appendleft(cmd[1])
+  elif cmd[0] == "push_back":
+    dq.append(cmd[1])
+  elif cmd[0] == "pop_front":
+    if len(dq) == 0 :
+      result.append(-1)
+    else:
+      result.append(dq.popleft())
+  elif cmd[0] == "pop_back":
+    if len(dq) == 0 :
+      result.append(-1)
+    else:
+      result.append(dq.pop())
+  elif cmd[0] == "size":
+    result.append(len(dq))
+  elif cmd[0] == "empty":
+    if len(dq) == 0 :
+      result.append(1)
+    else:
+      result.append(0)
+  elif cmd[0] == "front":
+    if len(dq) == 0 :
+      result.append(-1)
+    else:
+      result.append(dq[0])
+  elif cmd[0] == "back":
+    if len(dq) == 0 :
+      result.append(-1)
+    else:
+      result.append(dq[-1])
+
+for r in result:
+  print(r)
